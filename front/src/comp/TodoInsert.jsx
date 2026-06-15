8f27a7d04877f80752aa63a26e4de802e884ec9d
LEE YEWON <hol3161@gmail.com>
2026-06-15 11:49:46 +0900

    feat: update todo app UI and favicon

--- front/src/comp/TodoInsert.jsx
+++ front/src/comp/TodoInsert.jsx
@@ -16,11 +16,11 @@ function TodoInsert() {
     const today = new Date();
 
     const date = new Intl.DateTimeFormat("ko-KR", {
-      year: "numeric",
       month: "2-digit",
       day: "2-digit",
       hour: "2-digit",
       minute: "2-digit",
+      hour12: false,
     }).format(today);
 
     save({
